62b77a8c2146df1f82c5f2d30c5ac6883241f49f
kjeyers <kimberlyeyers2015@u.northwestern.edu>
2014-05-14 14:14:39 -0500

    Post-Work!
    Post-work!

--- script.js
+++ script.js
@@ -4,6 +4,29 @@
 
 // print data on page
 
+
+
+function init() {
+  Tabletop.init( { key: '0Auf85NGjrXTwdHpfSzhVeG9HX2NBaUNpNkMzZU9iWUE',
+                   callback: readData,
+                   simpleSheet: true } )
+}
+
+var cats;
+function readData(data, tabletop) {
+ console.log(data)
+ cats=data;
+ printData();
+}
+
+
+function printData() {
+	for (var i=0; i<cats.length; i++) {
+	$(".content").append("<h1>" + cats[i].name + "</h1> <img src= '" + cats[i].image + "'width='200' ><p>" + cats[i].description + "</p><hr>");
+	}
+}
+
 $(document).ready(function(){
+	init();
 
 })
