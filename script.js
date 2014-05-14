7dc36eba0a9eefb42718bf201f51fa3f5a2d0a0c
Pantsworth <nski6m@gmail.com>
2014-05-14 13:51:46 -0500

    More Joe
    There is more Joe now.

--- script.js
+++ script.js
@@ -4,6 +4,38 @@
 
 // print data on page
 
+function init() {
+  Tabletop.init( { key: '0AiG-VSZ2zFJ-dHlMdUJFVlVsNTl6QXZYMFlvVEdzaVE',
+                   callback: readData,
+                   simpleSheet: true } )
+}
+
+
+//read info
+var Joe;
+
+function readData(data, tabletop) { 
+	console.log(data) 
+	Joe = data;
+	printData();
+}
+
+function printData() {
+
+	for (var i=0; i<6; i++) {
+	$('.content').append("<h1>" + Joe[i].description + "</h1> <img src = '" + Joe[i].image + "'width='500' ><p>" + Joe[i].name + "</p><hr>");
+}
+}
+
+  // function showInfo(data, tabletop) {
+  //   alert("Successfully processed!")
+  //   console.log(data);
+  // }
+
+
 $(document).ready(function(){
+
+init();
+
 
 })
